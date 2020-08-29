1162d8949dae0b28a55d19588d75ac7f9c434589
Xanqus <sbk98031816@gmail.com>
2020-08-30 06:20:33 +0900

    2020-08-30

---
+++ db.js
@@ -0,0 +1,50 @@
+export const videos = [
+    {
+        id: 324393,
+        title: "Video awesome",
+        description: "This is something I love",
+        views: 24,
+        videoFile: "https://s3-us-west-2.amazonaws.com/s.cdpn.io/485050/movie.mp4",
+        creator: {
+            id: 121212,
+            name: "kbs",
+            email: "kb@s.com"
+        }
+    },
+    {
+        id: 1212121,
+        title: "Video super",
+        description: "This is something I love",
+        views: 24,
+        videoFile: "https://s3-us-west-2.amazonaws.com/s.cdpn.io/485050/movie.mp4",
+        creator: {
+            id: 121212,
+            name: "kbs",
+            email: "kb@s.com"
+        }
+    },
+    {
+        id: 555555,
+        title: "Video nice",
+        description: "This is something I love",
+        views: 24,
+        videoFile: "https://s3-us-west-2.amazonaws.com/s.cdpn.io/485050/movie.mp4",
+        creator: {
+            id: 121212,
+            name: "kbs",
+            email: "kb@s.com"
+        }
+    },
+    {
+        id: 1231241,
+        title: "Video perfect",
+        description: "This is something I love",
+        views: 24,
+        videoFile: "https://s3-us-west-2.amazonaws.com/s.cdpn.io/485050/movie.mp4",
+        creator: {
+            id: 121212,
+            name: "kbs",
+            email: "kb@s.com"
+        }
+    }
+]
